0fc15db1eb67fc8b2802931f149ecac75096c28d
Fritzgerrad <fritzgerrad.so@gmail.com>
2024-06-17 22:08:19 +0100

    Added the plain code for running solutions

---
+++ Test.py
@@ -0,0 +1,29 @@
+import unittest
+import solution
+class Tester(unittest.TestCase):
+    def testCode(self):
+        result1 = solution.Solution.run()
+        self.assertEqual(result1, )
+        
+        result2 = solution.Solution.run()
+        self.assertEqual(result2, )
+        
+        result3 = solution.Solution.run()
+        self.assertEqual(result3, )
+        
+        result4 = solution.Solution.run()
+        self.assertEqual(result4, )
+        
+        result5 = solution.Solution.run()
+        self.assertEqual(result5, )
+        
+        result6 = solution.Solution.run()
+        self.assertEqual(result1, )
+        
+        result6 = solution.Solution.run()
+        self.assertEqual(result6, )
+        
+        result7 = solution.Solution.run()
+        self.assertEqual(result7, )
+        
+unittest.main() 
---
+++ solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+
+    
+    def run(self):
+        pass
+        
+        
