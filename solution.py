6f17a1de4ef33c899417754fca196ca9dbc0a97b
Fritzgerrad <fritzgerrad.so@gmail.com>
2024-06-17 22:21:23 +0100

    Solved the problem to generate longest Unique Substring in a String

--- solution.py
+++ solution.py
@@ -1,7 +1,35 @@
 class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
 
+        if len(s) == 0:
+            return 0
+
+        diff = [1]
+        length = len(s)
+        substrings = []
+
+        #for substring in substrings:
+        for i in range(len(s)):
+            l = 0
+            for j in range(i,len(s)):
+                if s[j] not in s[i:j]:
+                    l += 1      
+
+                    if j == len(s) - 1:
+                        diff.append(l)
+
+                else:
+                    diff.append(l)
+                    break
+                                        
+        return max(diff)
+        
     
-    def run(self):
-        pass
+    def run(self,s):
+        return self.lengthOfLongestSubstring(s)
         
         
